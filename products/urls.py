07c01bb511cdd065fefc6c7a6865a0a238803e84
melony <hjz1010@gmail.com>
2022-07-25 13:52:02 +0900

    ADD: ListView에 paginator 추가

--- products/urls.py
+++ products/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     # path('/sub_category/<int:sub_category_id>', SubCategoryView.as_view()),
 
     path('', ListView.as_view()),
-    # 127.0.0.1:8000/products?category_id=2
-    # 127.0.0.1:8000/products?sub_category_id=7    
+    # 127.0.0.1:8000/products?category_id=2&page=2
+    # 127.0.0.1:8000/products?sub_category_id=7&page=1
+    # 127.0.0.1:8000/products?category_id=2&sub_category_id=7&page=1   ## 데이터 가공 시 sub_category_id만 필터링 함
 
     # path('/detail', DetailView.as_view()),
     path('/<int:product_id>', DetailView.as_view())
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 import json
 
-from django.http import JsonResponse
-from django.views import View
+from django.http           import JsonResponse
+from django.views          import View
+from django.core.paginator import Paginator
 
-from .models import Category, Furniture, ProductImage, SubCategory, Product, Brand, Color 
+from .models import *
 
 
 # class ListView_draft(View):
@@ -162,25 +163,27 @@ class ListView(View):
     def get(self, request):
         category_id     = request.GET.get('category_id', None)
         sub_category_id = request.GET.get('sub_category_id', None)
+        page_number     = request.GET.get('page', None)
 
-        products = Product.objects.filter(sub_category_id = sub_category_id) or Product.objects.filter(sub_category__category_id = category_id) 
-        product_list = []
-        for product in products:
-            product_list.append({
+        if int(category_id) in [c.id for c in Category.objects.all()] and int(sub_category_id) in [c.id for c in SubCategory.objects.all()]:
+            products = Product.objects.filter(sub_category_id = sub_category_id) or Product.objects.filter(sub_category__category_id = category_id) 
+            product_list = [{
                 'id'         : product.id,
                 'image'      : product.thumbnail_image_url,
                 'brandName'  : product.furniture.brand.name,
                 'productName': product.furniture.korean_name + '_' + product.color.korean_name,
                 'price'      : product.price
-            })
-        if product_list:
-            return JsonResponse({'message': 'SUCCESS', 'product_list': product_list}, status=200)
+            } for product in products]    
+
+            try: 
+                paginator    = Paginator(product_list, 4)
+                product_list = paginator.page(page_number).object_list
+                page_list    = list(paginator.page_range)
+                return JsonResponse({'message': 'SUCCESS', 'product_list': product_list, 'page_list': page_list}, status=200)
+            except :
+                return JsonResponse({'message': 'INVALID_PAGE'}, status=400)
         else:
             return JsonResponse({'message': 'INVALID_CATEGORY'}, status=400)
-
-
-
-
 
 
 class DetailView(View):
